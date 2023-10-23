dcfa1e8afac75bf19432cee889c701e215e96941
Hovhannes Amirjanyan <amirjanyanit@gmail.com>
2023-10-23 22:29:23 +0400

    users page

--- src/Pages/index.ts
+++
@@ -1,5 +0,0 @@
-export { Analytics } from "./Analytics";
-export { Report } from "./Report";
-export { User } from "./User";
-export { Login } from "./Login";
-export { NotFound } from "./NotFound";
